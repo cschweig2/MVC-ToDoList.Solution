7e3f0a84d3f6b12e6e6b8d2e1ab766b837abf1cc
Chelsea Becker <cschweig2@gmail.com>
2020-12-09 14:03:42 -0800

    add list functions to UI
    Co-authored-by: Patrick Osten <posten.coding@gmail.com>

--- ToDoList/Program.cs
+++ ToDoList/Program.cs
@@ -6,27 +6,52 @@ namespace ToDoList
 {
   public class Program
   {
+    public static void AddItem()
+    {
+      Console.WriteLine("Please enter the description for the new item.");
+      string description = Console.ReadLine();
+      Item newItem = new Item(description);
+      Console.WriteLine(description + " has been added to your list. Would you like to add an item to your list or view your list? (Add/View)");
+      string userInput = Console.ReadLine();
+      if (userInput == "View")
+      {
+        ViewItem();
+      }
+      else if (userInput == "Add")
+      {
+        AddItem();
+      }
+    }
+    public static void ViewItem()
+    {
+      List<Item> result = Item.GetAll();
+      foreach (Item item in result)
+      {
+        Console.WriteLine(item.Description);
+      }
+      Console.WriteLine("Would you like to add an item to your list or view your list? (Add/View)");
+      string userInput = Console.ReadLine();
+      if (userInput == "View")
+      {
+        ViewItem();
+      }
+      else if (userInput == "Add")
+      {
+        AddItem();
+      }
+    }
     public static void Main()
     {
-      List<Item> toDoList = new List<Item> { };
       Console.WriteLine("Welcome to the To Do List!");
       Console.WriteLine("Would you like to add an item to your list or view your list? (Add/View)");
       string userInput = Console.ReadLine();
       if (userInput == "Add")
       {
-        Console.WriteLine("Please enter the description for the new item.");
-        string description = Console.ReadLine();
-        Item newItem = new Item(description);
-        Console.WriteLine(description + " has been added to your list. Would you like to add an item to your list or view your list? (Add/View)");
-        userInput = Console.ReadLine();
+        AddItem();
       }
-      if (userInput == "View")
+      else if (userInput == "View")
       {
-        List<Item> result = Item.GetAll();
-        foreach (Item item in result)
-        {
-          Console.WriteLine(item.Description);
-        }
+        ViewItem();
       }
     }
   }
