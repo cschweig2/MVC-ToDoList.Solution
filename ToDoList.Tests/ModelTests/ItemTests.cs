cb93944b01fc25831c9a5b1627bfff34e802bcf6
Chelsea Becker <cschweig2@gmail.com>
2020-12-09 11:07:18 -0800

    add code for lesson 16
    Co-authored-by: Patrick Osten <posten.coding@gmail.com>

--- ToDoList.Tests/ModelTests/ItemTests.cs
+++ ToDoList.Tests/ModelTests/ItemTests.cs
@@ -47,5 +47,21 @@ namespace ToDoList.Tests
       List<Item> result = Item.GetAll();
       CollectionAssert.AreEqual(newList, result);
     }
+    [TestMethod]
+    public void GetAll_ReturnsItems_ItemList()
+    {
+      //Arrange
+      string description01 = "walk the dog.";
+      string description02 = "Wash the dishes";
+      Item newItem1 = new Item(description01);
+      Item newItem2 = new Item(description02);
+      List<Item> newList = new List<Item> {newItem1, newItem2 };
+
+      //Act
+      List<Item> result = Item.GetAll();
+
+      //Assert
+      CollectionAssert.AreEqual(newList, result);
+    }
   }
 }
