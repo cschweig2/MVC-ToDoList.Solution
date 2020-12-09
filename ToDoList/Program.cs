8d4b8909a2446bd8db19f9aaac8294454b0f1dc7
Chelsea Becker <cschweig2@gmail.com>
2020-12-09 11:53:04 -0800

    add basic UI functionality
    Co-authored-by: Patrick Osten <posten.coding@gmail.com>

--- ToDoList/Program.cs
+++ ToDoList/Program.cs
@@ -4,5 +4,27 @@ using ToDoList.Models;
 
 namespace ToDoList
 {
-  
+  public class Program
+  {
+    public static void Main()
+    {
+      // List<Item> _instances = new List<Item> { };
+      Console.WriteLine("Welcome to the To Do List!");
+      Console.WriteLine("Would you like to add an item to your list or view your list? (Add/View)");
+      string userInput = Console.ReadLine();
+      if (userInput == "Add")
+      {
+        Console.WriteLine("Please enter the description for the new item.");
+        string description = Console.ReadLine();
+        Item newItem = new Item(description);
+        Console.WriteLine(description + " has been added to your list. Would you like to add an item to your list or view your list? (Add/View)");
+        userInput = Console.ReadLine();
+      }
+      if (userInput == "View")
+      {
+        List<Item> result = Item.Description.GetAll();
+        Console.WriteLine(result.ToString());
+      }
+    }
+  }
 }
