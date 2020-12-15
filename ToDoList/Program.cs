8c56443c964c850c5eec594c394fa57cbc9baaae
Chelsea Becker <cschweig2@gmail.com>
2020-12-15 09:44:27 -0800

    add basic file tree
    Co-authored-by: Eric Stratton <strattonericj@gmail.com>

--- ToDoList/Program.cs
+++ ToDoList/Program.cs
@@ -1,58 +1,20 @@
-using System.Collections.Generic;
-using System;
-using ToDoList.Models;
+using System.IO;
+using Microsoft.AspNetCore.Hosting;
 
 namespace ToDoList
 {
   public class Program
   {
-    public static void AddItem()
+    public static void Main(string[] args)
     {
-      Console.WriteLine("Please enter the description for the new item.");
-      string description = Console.ReadLine();
-      Item newItem = new Item(description);
-      Console.WriteLine(description + " has been added to your list. Would you like to add an item to your list or view your list? (Add/View)");
-      string userInput = Console.ReadLine();
-      if (userInput == "View")
-      {
-        ViewItem();
-      }
-      else if (userInput == "Add")
-      {
-        AddItem();
-      }
-    }
-    public static void ViewItem()
-    {
-      List<Item> result = Item.GetAll();
-      foreach (Item item in result)
-      {
-        Console.WriteLine(item.Description);
-      }
-      Console.WriteLine("Would you like to add an item to your list or view your list? (Add/View)");
-      string userInput = Console.ReadLine();
-      if (userInput == "View")
-      {
-        ViewItem();
-      }
-      else if (userInput == "Add")
-      {
-        AddItem();
-      }
-    }
-    public static void Main()
-    {
-      Console.WriteLine("Welcome to the To Do List!");
-      Console.WriteLine("Would you like to add an item to your list or view your list? (Add/View)");
-      string userInput = Console.ReadLine();
-      if (userInput == "Add")
-      {
-        AddItem();
-      }
-      else if (userInput == "View")
-      {
-        ViewItem();
-      }
+      var host = new WebHostBuilder()
+        .UseKestrel()
+        .UseContentRoot(Directory.GetCurrentDirectory())
+        .UseIISIntegration()
+        .UseStartup<Startup>()
+        .Build();
+
+      host.Run();
     }
   }
 }
